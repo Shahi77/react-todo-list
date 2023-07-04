d306c691b38624b10ffcf85485681ed51560b406
Shahi77 <shahiinfinity02@gmail.com>
2023-07-04 11:59:26 +0530

    updating starting project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import React from 'react'
-import useStyles from './index.css';
-import Todo from './components/Todo';
-
 function App() {
-    return (
-
-        <div>
-            <h1>My Todos</h1>
-            <Todo text='Learn React' />
-            <Todo text='Master React' />
-            <Todo text='Explore React' />
-
-        </div>
-
-
-    )
+    return <div></div>;
 }
 
-export default App 
+export default App;
